bbd233e7030d9fdaa9e2ee7bf3df6673cb5a7f27
Halysson Freitas <halyssonfreitas@gmail.com>
2022-06-14 11:25:48 -0300

    :tada: Begin the project

---
+++ src/test/java/com/halysson/LearningTest/LearningTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halysson.LearningTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
